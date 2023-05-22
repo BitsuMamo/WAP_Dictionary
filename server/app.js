e74b90ea2e9217523c8afd8f95c2e25bfb89cc56
Bitsu <bitsua.mamo@gmail.com>
2023-05-21 23:36:36 -0500

    Added count route as well as changed /:word route to /getWord/:word

--- server/app.js
+++ server/app.js
@@ -10,14 +10,30 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/:word", async (req, res) => {
+
+
+app.get("/count", async (req, res) => {
+
+    const word = req.query.word;
+    if (word == null || word == "")
+        res.json(await prisma.entries.count())
+    else 
+        res.json(await prisma.entries.count({
+            where: {
+                word
+            }
+        }))
+    
+})
+
+app.get("/getWords/:word", async (req, res) => {
 
     let offset = req.query.offset ? Number(req.query.offset) : 0;
     let count = req.query.count ? Number(req.query.count) : 9;
 
     const word = req.params.word;
 
-    res.send(await prisma.entries.findMany({
+    res.json(await prisma.entries.findMany({
         where: {
             word
         },
@@ -32,7 +48,7 @@ app.get("/", async (req, res) => {
     let offset = req.query.offset ? Number(req.query.offset) : 0;
     let count = req.query.count ? Number(req.query.count) : 9;
 
-    res.send(await prisma.entries.findMany({
+    res.json(await prisma.entries.findMany({
         skip: offset,
         take: count
     }));
